cd2c11acaf5d566fce736ea96778e00f64e67b1a
Richard Parra <richardparra@hotmail.com>
2024-08-02 16:36:43 +0000

    20240802

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+import { environments } from './environments';
+import config from './config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      expandVariables: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DATABASE_DBNAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
